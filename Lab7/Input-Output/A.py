9fbd86b1acf5beb6e2935d74070a02998aaab30b
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2023-03-22 16:51:35 +0600

    Add files via upload

---
+++ Lab7/Input-Output/A.py
@@ -0,0 +1,5 @@
+import math
+a = int(input())
+b = int(input())
+ans = math.sqrt((a**2) + (b**2))
+print(ans)
---
+++ Lab7/Input-Output/B.py
@@ -0,0 +1,6 @@
+number = int(input())
+print("The next number for the number " + str(number) + " is " + str(number + 1) + ".")
+print("The previous number for the number " + str(number) + " is " + str(number - 1) + ".")
+
+#print(f"The next number for the number {number} is {number+1}.")
+#print(f"The previous number for the number {number} is {number-1}.")
